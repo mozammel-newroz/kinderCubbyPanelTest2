e20bb1acb958bce1af5ca5e9551e5144a20a2776
mozammel-newroz <mozammel@newroztech.com>
2021-07-14 12:57:59 +0600

    testing

--- src/routes/Pages/Test.jsx
+++ src/routes/Pages/Test.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Test = () => {
-    return (
-        <div>
-            <h>Test</h>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>Tested by Rubel</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere ut placeat assumenda. Dolorem optio ea odio
+        dolorum distinctio nesciunt fugiat officia, possimus recusandae ratione eum maiores perferendis vitae suscipit!
+      </p>
+    </div>
+  );
+};
 
-export default Test
+export default Test;
